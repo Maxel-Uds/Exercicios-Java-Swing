270a64a9c5b2ad428721de2841de0cc5dc26079c
Maxel-Uds <maxellopes32@gmail.com>
2022-06-13 17:44:27 -0300

    Adicionando instruções antes de começar o jogo

--- JogoDaMemoria/src/graphic/Game.java
+++ JogoDaMemoria/src/graphic/Game.java
@@ -12,6 +12,12 @@ public class Game extends JFrame {
         super("Jogo da Memória");
         UIManager.put("OptionPane.messageFont", new FontUIResource(new Font("Arial", Font.PLAIN, 24)));
 
+        JOptionPane.showMessageDialog(null, "Acerte os pares escondidos " +
+                "\nVocê terá 5 segundos pra visualizar as peças " +
+                "\nCada acerto te dá 5 pontos e cada erro tira 3 pontos",
+                "Intruções",
+                JOptionPane.INFORMATION_MESSAGE);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600, 600);
         setLocation(400, 200);
